5e1277fe2f52f6cf2def331679bafc94644b8103
NegiSushant <sushantsinghnegi1998@gmail.com>
2025-09-15 16:43:19 +0530

    cabin feature implementing

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,3 +1,7 @@
 export async function getCabins() {}
 
 export async function deleteCabin(id) {}
+
+export async function createCabin(newCabin) {}
+
+export async function createEditCabin(newCabin, id) {}
